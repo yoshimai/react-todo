e2d3ad914ea00a636f20b4d5587c95d30f96fcd6
yoshimai <117501051+yoshimai@users.noreply.github.com>
2022-11-11 15:03:45 +0900

    mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["abc", "def"]);
+  const [completeTodos, setCompleteTodo] = useState(["ghi"]);
   return (
     <>
-      <div>
+      <div className="input_area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
-            <li>abc</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
-      <div>
-        <p>完了したTODO</p>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
         <ul>
-          <div>
-            <li>def</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
